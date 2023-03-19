d5cf3d88d6c048fe2a911fb92c46d52075b57542
raimess11 <rahma.sakti24@gmail.com>
2023-03-20 04:38:28 +0700

    menambahkan class HeloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Generic!");
+
+        HeloGeneric G = new HeloGeneric();
+        G.SapaUser<string>("Raimessnyazeta");
+    }
+
+    class HeloGeneric
+    {
+        public void SapaUser<type>(type x) 
+        {
+            Console.WriteLine("Halo User "+(dynamic) x );
+        }
+    }
+}
